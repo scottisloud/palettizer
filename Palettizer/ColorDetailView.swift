93466c2ff4e45ef99a6f78a7e0a4fcf9e5027131
Scott Lougheed <scott@scottlougheed.com>
2019-06-05 14:56:35 -0400

    Fixed some layout issues (more work needed) and added copy hex feature

--- Palettizer/ColorDetailView.swift
+++ Palettizer/ColorDetailView.swift
@@ -13,22 +13,34 @@ class ColorDetailView: UIViewController {
 	@IBOutlet var colorChip: UIView!
 	@IBOutlet var colorName: UILabel!
 	@IBOutlet var colorHex: UILabel!
-	@IBOutlet var colorRGBA: UILabel!
+	@IBOutlet var copyInstructions: UILabel!
 	
 	var selectedColorName: String?
 	var selectedColorHex: String?
 	var selectedColorRGBA: String?
 	
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		self.title = selectedColorName
 		navigationController?.navigationBar.prefersLargeTitles = false
+		
+		copyInstructions.text = "Tap the hex value to copy it to your clipboard"
+		
+		colorHex.isUserInteractionEnabled = true
+		let recognizer = UITapGestureRecognizer(target: self, action: #selector(hexTapped))
+		colorHex.addGestureRecognizer(recognizer)
 		if let c = selectedColorHex {
 			let bg = UIColor(hex: c)
 			colorChip.backgroundColor = bg
 		}
 		colorName.text = selectedColorName
 		colorHex.text = selectedColorHex
-		colorRGBA.text = "RGBA"
+	}
+	
+	@objc func hexTapped() {
+		UIPasteboard.general.string = colorHex.text
+		print(UIPasteboard.general.string!)
 	}
 }
